ec5c77c99c188f21a7dcab3bcb85774b60e7b8f2
Florian Louvet <flouvet@greenbull.group>
2022-12-05 16:06:24 +0100

    Add i18n translations for chat placeholder

--- src/Translations/resources/en.ts
+++ src/Translations/resources/en.ts
@@ -1,6 +1,9 @@
 export default {
   welcome: "Just pretending I'm lauching a huge app...",
-  actions: {
-    continue: 'Continue',
+  chat: {
+    placeholder: {
+      empty: 'No messages yet, write something!',
+      error: 'An unknown error occured',
+    },
   },
 }
