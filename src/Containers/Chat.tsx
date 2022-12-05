fab282129d227c148f61923008b10ae62eb5f712
Florian Louvet <flouvet@greenbull.group>
2022-12-05 14:17:04 +0100

    Update Chat into a basic message list

--- src/Containers/Chat.tsx
+++ src/Containers/Chat.tsx
@@ -6,10 +6,19 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
+  FlatList,
+  SafeAreaView,
 } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import { useTheme } from '@/Hooks'
 import { useLazyFetchOneQuery } from '@/Services/modules/users'
+import Message from '@/Types/chat/Message'
+import MessageRow from '@/Components/molecules/chat/MessageRow'
+
+// TODO: Move fake data
+const DATA: [Message] = [
+  { id: 0, author: { id: 0, name: 'Bot' }, content: 'Hello I am a bot' },
+]
 
 const Chat = () => {
   const { t } = useTranslation()
@@ -19,7 +28,18 @@ const Chat = () => {
   const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
     useLazyFetchOneQuery()
 
-  return <>{/* TODO: ChatFlatlist*/}</>
+  // Chat message renderer
+  const renderItem = (message: Message) => <MessageRow message={message} />
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => renderItem(item)}
+        keyExtractor={(item: Message) => item.id.toString()}
+      />
+    </SafeAreaView>
+  )
 }
 
 export default Chat
