4ffb8e4114d0ac837b9c5c5235dec69de1663195
Florian Louvet <flouvet@greenbull.group>
2022-12-05 17:15:51 +0100

    WIP: Update Chat to send a message

--- src/Containers/Chat.tsx
+++ src/Containers/Chat.tsx
@@ -3,24 +3,33 @@ import { ActivityIndicator, FlatList, SafeAreaView } from 'react-native'
 import { useTheme } from '@/Hooks'
 import Message from '@/Types/chat/Message'
 import MessageRow from '@/Components/molecules/chat/MessageRow'
-import { getChatMessages } from '@/Services/modules/chat/chat'
+import { createChatMessage, getChatMessages } from '@/Services/modules/chat/chat'
 import { useTranslation } from 'react-i18next'
 import Placeholder from '@/Components/molecules/Placeholder'
+import MessageComposer from '@/Components/molecules/chat/MessageComposer'
 
 /**
  * Render a list of message (or a placeholder) and a text input
  */
 const Chat = () => {
   const { t } = useTranslation()
-  const { Gutters, Layout } = useTheme()
+  const { Gutters, Layout, Colors } = useTheme()
 
   const [messages, setMessages] = useState<Array<Message> | null>()
-  const [error, setError] = useState<Boolean>(false) // TODO: Set back to false after testing
+  const [error, setError] = useState<Boolean>(false)
 
   const fetchMessages = useCallback(async () => {
     await getChatMessages()
       .then(r => setMessages(r))
       .catch(e => setError(e))
+  }, [])
+
+  const sendMessage = useCallback(async (text: String) => {
+    console.debug('send message:', text)
+    await createChatMessage(text).then(
+      (success: Boolean) =>
+        !success && console.warn('An error occured, print a toast!'),
+    )
   }, [])
 
   // Initial messages loading effect
@@ -47,20 +56,20 @@ const Chat = () => {
   }, [error, t])
 
   return (
-    <SafeAreaView
-      style={[Layout.fill, Gutters.largeVPadding, Gutters.largeHPadding]}
-    >
+    <SafeAreaView style={[Layout.fill, { backgroundColor: Colors.background }]}>
       {messages || error ? (
         <FlatList
           data={messages}
           renderItem={({ item }) => renderItem(item)}
           keyExtractor={(item: Message) => item.id.toString()}
           ListEmptyComponent={renderPlaceholder}
+          style={[Gutters.regularVPadding, Gutters.regularHPadding]}
           inverted={!error}
         />
       ) : (
         <ActivityIndicator size={'large'} style={[Gutters.largeVMargin]} />
       )}
+      <MessageComposer onDone={(text: String) => sendMessage(text)} />
     </SafeAreaView>
   )
 }
