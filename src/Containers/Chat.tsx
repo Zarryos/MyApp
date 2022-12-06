eae2b6d351422a9463e42b716e6be85be70e36ae
Florian Louvet <flouvet@greenbull.group>
2022-12-06 12:17:02 +0100

    Use the redux store from the Chat component

--- src/Containers/Chat.tsx
+++ src/Containers/Chat.tsx
@@ -3,10 +3,15 @@ import { ActivityIndicator, FlatList, SafeAreaView } from 'react-native'
 import { useTheme } from '@/Hooks'
 import Message from '@/Types/chat/Message'
 import MessageRow from '@/Components/molecules/chat/MessageRow'
-import { createChatMessage, getChatMessages } from '@/Services/modules/chat/chat'
+import {
+  createChatMessage,
+  getChatMessages,
+} from '@/Services/modules/chat/chat'
 import { useTranslation } from 'react-i18next'
 import Placeholder from '@/Components/molecules/Placeholder'
 import MessageComposer from '@/Components/molecules/chat/MessageComposer'
+import { useSelector } from 'react-redux'
+import { RootStateOrAny } from 'react-redux'
 
 /**
  * Render a list of message (or a placeholder) and a text input
@@ -15,21 +20,18 @@ const Chat = () => {
   const { t } = useTranslation()
   const { Gutters, Layout, Colors } = useTheme()
 
-  const [messages, setMessages] = useState<Array<Message> | null>()
+  // TODO: Improve error handling with an Error type and proper message display
   const [error, setError] = useState<Boolean>(false)
 
+  // Listen on messages from the redux store
+  const messages = useSelector<RootStateOrAny>(store => store.chat.messages)
+
   const fetchMessages = useCallback(async () => {
-    await getChatMessages()
-      .then(r => setMessages(r))
-      .catch(e => setError(e))
+    await getChatMessages().then((success: Boolean) => setError(!success))
   }, [])
 
   const sendMessage = useCallback(async (text: String) => {
-    console.debug('send message:', text)
-    await createChatMessage(text).then(
-      (success: Boolean) =>
-        !success && console.warn('An error occured, print a toast!'),
-    )
+    await createChatMessage(text).then((success: Boolean) => setError(!success))
   }, [])
 
   // Initial messages loading effect
