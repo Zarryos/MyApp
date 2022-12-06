b35476c1c2d5a98e64be04f19c0b27ae8d301d6a
Florian Louvet <flouvet@greenbull.group>
2022-12-06 13:50:23 +0100

    Add message generator

--- src/Utils/fakeData/message.tsx
+++ src/Utils/fakeData/message.tsx
@@ -1,4 +1,21 @@
+import { store } from '@/Store'
+import { addMessage } from '@/Store/Redux/actions/message'
+import Message from '@/Types/chat/Message'
+import { contactFakeUser } from './user'
+
 const usedMessageIds: Array<number> = [0]
+const availableMessagesContent: Array<String> = [
+  'You think so?',
+  "You've got fat.",
+  'Where is your favorite hammer now?',
+  'Snap!',
+  "You're a boring god",
+  'Tell me more',
+  'I could use a drink',
+  "Avengers, please don't assemble!",
+  'My favorite god has always being Loki',
+  'Love the new haircut!',
+]
 
 /**
  * Generate a number that is not used by any other messages
@@ -9,4 +26,31 @@ export function generateMessageId() {
   const availableId: number = Math.max(...usedMessageIds) + 1
   usedMessageIds.push(availableId)
   return availableId
+}
+
+/**
+ * Burn a string from an array
+ * @returns A random message content
+ */
+function generateMessageText(): String {
+  // Get a random index
+  const i = Math.floor(Math.random() * availableMessagesContent.length)
+
+  // Return the random element
+  return availableMessagesContent[i]
+}
+
+/**
+ * Generate a message
+ */
+export function generateChatMessageAsContact() {
+  const contactMessage: Message = {
+    id: generateMessageId(),
+    dateString: new Date().toLocaleDateString('en-US'),
+    content: generateMessageText(),
+    author: contactFakeUser,
+  }
+
+  // Success! Dispatch created message in redux
+  store.dispatch(addMessage(contactMessage))
 }
