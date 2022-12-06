49a3ea855cee90f86277cd53eb69e0c6ee9c9dc1
Florian Louvet <flouvet@greenbull.group>
2022-12-06 12:15:56 +0100

    Improve message row

--- src/Components/molecules/chat/MessageRow.tsx
+++ src/Components/molecules/chat/MessageRow.tsx
@@ -1,16 +1,31 @@
+import useTheme from '@/Hooks/useTheme'
 import Message from '@/Types/chat/Message'
-import React from 'react'
+import { myFakeUser } from '@/Utils/fakeData/user'
+import React, { useMemo } from 'react'
 import { View, Text } from 'react-native'
 
 /**
  * Render a message in a chat
  * @param props message, the message to display
  */
-const MessageRow = (props: { message: Message }) => (
-  <View>
-    <Text>{props.message.author.name}</Text>
-    <Text>{props.message.content}</Text>
-  </View>
-)
+const MessageRow = (props: { message: Message }) => {
+  const { Layout, Fonts, Gutters } = useTheme()
+
+  /**
+   * Return true if the message author pocess my the connected user id, false otherwise
+   */
+  const isMyMessage: Boolean = useMemo(() => {
+    return props.message.author.id === myFakeUser.id
+  }, [props.message.author.id])
+
+  return (
+    <View style={isMyMessage ? Layout.alignItemsEnd : Layout.alignItemsStart}>
+      <Text style={[Fonts.textRegular, Gutters.smallVMargin]}>
+        {props.message.author.name}
+      </Text>
+      <Text style={Fonts.textSmall}>{props.message.content}</Text>
+    </View>
+  )
+}
 
 export default MessageRow
